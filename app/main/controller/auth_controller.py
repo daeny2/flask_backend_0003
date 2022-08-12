b43f9aa561e1bd8548318228f85e9ab11f7d6601
Daeny Lee <daeny2@gmail.com>
2022-08-12 13:51:52 +0900

    update code

--- app/main/controller/auth_controller.py
+++ app/main/controller/auth_controller.py
@@ -8,14 +8,13 @@ from typing import Dict, Tuple
 api = AuthDto.api
 user_auth = AuthDto.user_auth
 
-
 @api.route('/login')
 class UserLogin(Resource):
     # User Login Resource
     @api.doc('user login')
     @api.expect(user_auth, validate=True)
     def post(self) -> Tuple[Dict[str, str], int]:
-        # get the post data
+        """get the post data"""
         post_data = request.json
         return Auth.login_user(data=post_data)
 
@@ -25,6 +24,6 @@ class LogoutAPI(Resource):
     # Logout Resource
     @api.doc('logout a user')
     def post(self) -> Tuple[Dict[str, str], int]:
-        # get auth token
+        """get auth token"""
         auth_header = request.headers.get('Authorization')
         return Auth.logout_user(data=auth_header)
